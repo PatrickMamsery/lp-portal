7f0fc6a6ec3c1f56a631cc28ce6977694296005d
PatrickMamsery <pkrobert1612@gmail.com>
2024-05-08 22:32:30 +0300

    feat: setup resources to create entries, test run done, pending subtopics

--- app/Models/Competence.php
+++ app/Models/Competence.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Competence extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'subject_id',
+        'topic_id',
+        'school_id',
+    ];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
 }
